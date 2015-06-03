d27df88a3cca45bdc69bf7bdb97e549b4bf87307
Johan Nyström <johannystrom85@gmail.com>
2015-06-03 11:42:17 +0200

    changes

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
         //db: 'mongodb://localhost/multivision',
         db: 'mongodb://andreas:andreas@ds051831.mongolab.com:51831/heroku_app34590524',
         rootPath: rootPath,
-        port: process.env.PORT || 3030
+        port: process.env.PORT || 80
     },
     production:{
         db: 'mongodb://andreas:andreas@ds051831.mongolab.com:51831/heroku_app34590524',
